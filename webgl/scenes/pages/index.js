92c7d2699c926661236cf9b7e7c060bd7882a8fc
Donald Dong <mail@ddong.me>
2022-04-29 20:43:54 -0700

    Add link to index

--- webgl/scenes/pages/index.js
+++ webgl/scenes/pages/index.js
@@ -4,9 +4,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="container">
-      <Link href="/scenes/box-demo">
-        <a>Box Demo Scene</a>
-      </Link>
+      <Link href="/scenes/box-demo"><a>Box Demo Scene</a></Link>
+      <br/>
+      <Link href="/scenes/box-demo-2"><a>Box Demo Scene 2: Camera</a></Link>
     </div>
   );
 };
